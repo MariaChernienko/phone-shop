2741f03a9a95f3e756e4f699360e8f1c69688bfa
Maria <murikmarusia@gmail.com>
2019-01-18 15:34:50 +0200

    added components

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-console.log('Hello, phone shop!!');
+
+const header = new Header({
+  element: document.querySelector('.header'),
+});
+
+const basket = new Basket({
+  element: document.querySelector('.basket'),
+  title: 'Shopping Cart',
+  items: [
+    'Samsung Galaxy S9',
+    'Samsung Galaxy S9',
+    'Samsung Galaxy S9',
+    'Samsung Galaxy S9',
+  ],
+  open: document.querySelector('.header__basket'),
+});
+
+const shop = new Goods({
+  element: document.querySelector('.content'),
+  items: getServices,
+});
